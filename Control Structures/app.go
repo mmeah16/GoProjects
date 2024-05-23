37d33dc7825cbac563f670da3fa7f074e05e4af7
Muntasir Meah <muntasirmeah@Muntasirs-Air.home>
2024-05-23 16:21:10 -0400

    error handling supported

--- Control Structures/app.go
+++ Control Structures/app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -10,11 +11,22 @@ import (
 
 const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() float64 {
-	data, _ := os.ReadFile(accountBalanceFile)
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalanceFile)
+
+	// if
+	if err != nil {
+		return 1000, errors.New("Failed to find account balance!")
+	}
+
 	balanceText := string(data)
-	balance, _ := strconv.ParseFloat(balanceText, 64)
-	return balance
+	balance, err := strconv.ParseFloat(balanceText, 64)
+
+	if err != nil {
+		return 1000, errors.New("Failed to parse stored account balance!")
+	}
+
+	return balance, nil
 }
 
 func writeBalanceToFile(balance float64) {
@@ -23,8 +35,13 @@ func writeBalanceToFile(balance float64) {
 }
 
 func main() {
-	var accountBalance = getBalanceFromFile()
+	var accountBalance, err = getBalanceFromFile()
 
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("------")
+	}
 	for i := 0; i < 200; i++ {
 
 		fmt.Println("Welcome to Go Bank!")
